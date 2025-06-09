fa4b9f9e0080e3d39e47b3eb815ba518b4571867
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-06-09 22:39:53 +0800

    fix: adjust layout dimensions in Home component for improved responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import StackExp from '@/components/StackExp';
-import Hero from '../components/Hero';
-import Projects from '@/components/Projects';
-import Contact from '@/components/Contact';
+import StackExp from "@/components/StackExp";
+import Hero from "../components/Hero";
+import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
 
 export default function Home() {
-  return (
+	return (
 		<main className="relative bg-[#121212] flex flex-col justify-center items-center overflow-hidden mx-auto">
-			<div className="max-w-7xl w-full">
+			<div className="max-w-6xl w-full h-screen snap-y overflow-scroll no-scrollbar">
 				<Hero />
 				<StackExp />
 				<Projects />
