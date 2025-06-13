618e047d1689e2d8749f0c6619b9b1a2b3970601
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-06-14 04:10:18 +0800

    feat: update padding and margin in StackExp component for improved layout consistency

--- components/StackExp.tsx
+++ components/StackExp.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 const StackExp = () => {
   return (
 		<div className="flex flex-col h-screen w-full snap-center">
-			<div className="font-primary text-white text-6xl mt-10">
+			<div className="font-primary text-white text-6xl p-6 md:p-8">
 				<span className="text-(--grey)">Tech</span> Stack
 			</div>
-			<div className="font-secondary text-white text-xl text-center">
+			<div className="font-secondary text-white text-lg md:text-xl text-center flex-1 flex items-center p-12 md:p-20">
 				Adaptable and quick to master new tools and frameworks, I ensure my tech
 				stack evolves with industry trends and continuously refine my approach
 				to build efficient, modern websites.
