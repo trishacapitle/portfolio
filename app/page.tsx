95f85c29fefc7425b5f680d18d9ba3200e259874
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-06-14 04:10:27 +0800

    feat: update layout structure in Home component for improved responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,14 @@ import StackExp from "@/components/StackExp";
 import Hero from "../components/Hero";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
 	return (
 		<main className="relative bg-[#121212] flex flex-col justify-center items-center overflow-hidden mx-auto">
-			<div className="max-w-6xl w-full h-screen snap-y overflow-scroll no-scrollbar scroll-smooth snap-proximity">
+			
+			<div className="max-w-7xl w-screen h-screen snap-y overflow-scroll no-scrollbar scroll-smooth">
+				<Navbar />
 				<Hero />
 				<StackExp />
 				<Projects />
