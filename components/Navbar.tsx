1b88e1f694648e05f342912f96fce5b3f66e91a4
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-06-14 23:39:49 +0800

    feat: update Navbar component layout and structure for improved spacing and hover effects

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,16 +2,21 @@ import React from "react";
 
 const Navbar = () => {
 	return (
-		<div className="w-full h-10 justify-between items-center flex p-4 md:p-6 snap-center">
-			<div className="font-primary text-white">
+		<div className="w-full h-16 justify-between items-center flex p-6 md:p-8 snap-center">
+			<div className="font-primary text-white text-xl">
 				<span className="text-(--grey)">Ash</span>Dev
 			</div>
-			<div className="flex gap-6 text-xs md:text-sm font-secondary text-(--grey)">
-				<p>About</p>
-				<p>Experience</p>
-				<p>Projects</p>
-				<p>Contact</p>
-			</div>
+			<ul className="flex gap-6 text-xs md:text-sm font-secondary text-(--grey) ">
+				<li className="hover:text-white duration-100 ease-in-out py-2">
+					About
+				</li>
+				<li className="hover:text-white duration-100 ease-in-out py-2">
+					Experience
+				</li>
+				<li className="hover:text-white duration-100 ease-in-out py-2">
+					Projects
+				</li>
+			</ul>
 		</div>
 	);
 };
