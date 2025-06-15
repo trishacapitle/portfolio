0c8f0c983491ce6959349e3bf294fc3275f32a84
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-06-15 18:10:44 +0800

    feat: replace StackExp component with TechStack for improved code clarity

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import StackExp from "@/components/StackExp";
-import Hero from "../components/Hero";
+import TechStack from "@/components/TechStack";
+import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
 import Navbar from "@/components/Navbar";
@@ -11,7 +11,7 @@ export default function Home() {
 			<div className="max-w-7xl w-screen h-screen snap-y overflow-scroll no-scrollbar scroll-smooth">
 				<Navbar />
 				<Hero />
-				<StackExp />
+				<TechStack />
 				<Projects />
 				<Contact />
 			</div>
