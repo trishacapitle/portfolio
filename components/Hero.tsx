8cb19e66177a13a894fc8415baa9965cfda4d0a1
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-06-15 22:43:57 +0800

    feat: add draggable attribute to Image component in Hero for improved user interaction

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -129,6 +129,7 @@ const Hero = () => {
 					width={800}
 					height={800}
 					alt="Picture of the author"
+					draggable={false}
 					className="object-cover w-full h-full absolute bottom-0"
 				/>
 				<button className="font-primary text-white text-2xl md:text-3xl flex items-center gap-1 md:gap-2 bg-gray-600/10 backdrop-filter backdrop-blur-sm border border-gray-500 rounded-lg p-3 md:p-4 md:px-6 hover:bg-gray-400/25 transition-colors duration-300 absolute bottom-30 right-40 md:bottom-40 md:right-60 z-99">
