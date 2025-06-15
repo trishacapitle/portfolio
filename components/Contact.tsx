52ac05906e877048ef5adf5dcf041e7d1f95ffe8
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-06-16 05:00:55 +0800

    feat: initial Contact component layout and styling

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,27 +1,54 @@
 import React from "react";
 
 const Contact = () => {
-	return (
-		<div className="flex h-screen w-full">
-			<div className="font-primary text-white mt-10">
-				<div className="text-6xl">
-					<span className="text-(--grey)">Let&apos;s</span> Connect
-				</div>
-				<p className="text-(--grey) font-secondary">
-					Say hello at{" "}
-					<span className="text-white underline">
-						trishacapitle01@gmail.com
-					</span>
-				</p>
-				<p className="text-(--grey) font-secondary">
-					For more info, here&apos;s my{" "}
-					<span className="text-white underline">resume</span>
-				</p>
-				<div className="flex gap-6 mt-10">
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="h-200 w-full">
+      <div className="flex flex-1 flex-col md:flex-row">
+        <div className="section mt-10 flex flex-col text-white">
+          <p className="font-primary text-6xl">
+            <span className="text-(--grey)">Let&apos;s</span> Connect
+          </p>
+          <p className="font-secondary text-(--grey)">
+            Say hello at{" "}
+            <span className="text-white underline">
+              trishacapitle01@gmail.com
+            </span>
+          </p>
+          <p className="font-secondary text-(--grey)">
+            For more info, here&apos;s my{" "}
+            <span className="text-white underline">resume</span>
+          </p>
+          <div>
+            <a href="">Linkedin Icon</a>
+            <a href="">GithubIcon</a>
+          </div>
+          <p>C 2025 AshDev</p>
+        </div>
+        <div className="form font-secondary flex flex-col text-white">
+          <label htmlFor="name">Name</label>
+          <input id="name" type="text" placeholder="John Doe" />
+          <label htmlFor="email">Email</label>
+          <input id="email" type="text" placeholder="johndoe@email.com" />
+          <label htmlFor="subject">Subject</label>
+          <input id="subject" type="text" />
+          <label htmlFor="message">Message</label>
+          <input id="message" type="text" />
+          <button>Send Message paperplane here</button>
+        </div>
+      </div>
+      <div>
+        <hr className="border border-[#141414]" />
+        <p className="font-secondary text-center text-(--grey)">
+          Designed in <span className="text-white">Figma</span>, coded on{" "}
+          <span className="text-white">Visual Studio Code</span>. Built with{" "}
+          <span className="text-white">Next.js</span> and{" "}
+          <span className="text-white">Tailwind CSS</span>, animations brewed on{" "}
+          <span className="text-white">GSAP</span>, deployed with{" "}
+          <span className="text-white">Vercel</span>.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
