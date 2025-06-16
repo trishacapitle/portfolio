e2540aae48f0258330508d09c0f365280a7fb2e3
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-06-17 04:15:21 +0800

    feat: applied initial styling to button, will adjsut the title and icon of the button

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-
 const Button = (title: string, icon: string ) => {
 
 
   return (
-    <div>{title} : { icon}</div>
+    <button className="font-primary text-white text-2xl md:text-3xl flex items-center gap-1 md:gap-2 bg-gray-600/10 backdrop-filter backdrop-blur-sm border border-gray-500 rounded-lg p-3 md:p-4 md:px-6 hover:bg-gray-400/25 transition-colors duration-300 absolute bottom-30 right-40 md:bottom-40 md:right-60 z-99">{title} {icon} </button>
   )
 }
 
