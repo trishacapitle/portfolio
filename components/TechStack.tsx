5df1fe275f9f562f9cb5e074d1111b59641b3e10
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-06-19 04:53:48 +0800

    fix: remove unnecessary line breaks in TechStack component

--- components/TechStack.tsx
+++ components/TechStack.tsx
@@ -79,8 +79,7 @@ const TechStack = () => {
 			</div>
 
 			<div
-				
-				className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 p-4"
+				className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 p-4" 
 			>
 				{icons.map((icon) => (
 					<div
