616b490e0848b49be34ee043505794f978355da7
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-06-19 07:05:09 +0800

    fix: remove unnecessary whitespace in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import Navbar from "@/components/Navbar";
 export default function Home() {
 	return (
 		<main className=" bg-[#121212] flex flex-col justify-center items-center overflow-hidden mx-auto">
-			
 			<div className="max-w-7xl w-screen no-scrollbar">
 				<Navbar />
 				<Hero />
