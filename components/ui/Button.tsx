b2200206dd82f2c6bcbc5b724ddfc1c6654026e4
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-06-21 03:01:37 +0800

    refactor: update Button component to use TypeScript interfaces for props

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,9 +1,20 @@
-const Button = (title: string, icon: string ) => {
+import { ReactNode } from "react";
 
-
-  return (
-    <button className="font-primary text-white text-2xl md:text-3xl flex items-center gap-1 md:gap-2 bg-gray-600/10 backdrop-filter backdrop-blur-sm border border-gray-500 rounded-lg p-3 md:p-4 md:px-6 hover:bg-gray-400/25 transition-colors duration-300 absolute bottom-30 right-40 md:bottom-40 md:right-60 z-99">{title} {icon} </button>
-  )
+interface ButtonProps {
+  title: string;
+  icon?: ReactNode;
+  onClick?: () => void;
 }
 
-export default Button
+const Button: React.FC<ButtonProps> = ({ title, icon, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="font-primary absolute right-40 bottom-30 z-99 flex items-center gap-1 rounded-lg border border-gray-500 bg-gray-600/10 p-3 text-2xl text-white backdrop-blur-sm backdrop-filter transition-colors duration-300 hover:bg-gray-400/25 md:right-60 md:bottom-40 md:gap-2 md:p-4 md:px-6 md:text-3xl"
+    >
+      {title} {icon}
+    </button>
+  );
+};
+
+export default Button;
