300c4872995752ba23b4f64151c309cf024d3b01
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-06-21 03:31:35 +0800

    refactor: enhance Button component to accept className prop for better styling flexibility

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -3,14 +3,15 @@ import { ReactNode } from "react";
 interface ButtonProps {
   title: string;
   icon?: ReactNode;
+  className: string;
   onClick?: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ title, icon, onClick }) => {
+const Button: React.FC<ButtonProps> = ({ title, icon, className, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="font-primary absolute right-40 bottom-30 z-99 flex items-center gap-1 rounded-lg border border-gray-500 bg-gray-600/10 p-3 text-2xl text-white backdrop-blur-sm backdrop-filter transition-colors duration-300 hover:bg-gray-400/25 md:right-60 md:bottom-40 md:gap-2 md:p-4 md:px-6 md:text-3xl"
+      className={`font-primary flex items-center gap-2 rounded-lg border border-gray-500 bg-gray-600/25 p-3 leading-0 text-white backdrop-blur-sm backdrop-filter transition-colors duration-300 hover:bg-gray-400/25 md:gap-4 md:py-4 md:px-6  ${className}`}
     >
       {title} {icon}
     </button>
