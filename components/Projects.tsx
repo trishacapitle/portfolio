6d192361bc286be1a4f583a4be6cab2aec1f15eb
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-06-23 10:01:27 +0800

    feat: update Projects component with detailed project descriptions and add new projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,29 +3,68 @@ import ProjectCard from "./ui/ProjectCard";
 const Projects = () => {
   const projects = {
     project1: {
-      title: "Project One",
+      title: "Digital Tour Guide System for a Prototype Electric Vehicle",
       description:
-        "This is a description of project one. It does many things and is very useful.",
-      stack: "React, Node.js, Express",
+        "Developed a digital tour guide solution of a touring e-shuttle that automatically narrates destination highlights based on geofence triggers.",
+      stack: "Electron, React, Tailwind",
       image: "/portrait.png",
-      tags: "Conceptual Work",
+      tags: "Contract",
       preview: "https://example.com/preview1",
-      github: "https://github.com/example/project-one",
     },
     project2: {
-      title: "Project Two",
+      title:
+        "Battery and Solar Monitoring System with Custom Android App Syncing",
       description:
-        "This is a description of project two. It is even better than the first one.",
-      stack: "Vue, Django",
+        "Developed an all in one battery and solar monitoring system for a prototype electric vehicle that have sync functionality with it's custom anroid app.",
+      stack: "Electron, React, React Native, Tailwind",
       image: "/portrait.png",
-      tags: "Conceptual Work",
+      tags: "Contract",
+      preview: "https://example.com/preview1",
+      github: "https://github.com/example/project-two",
+    },
+    project3: {
+      title: "Creative Landing Page",
+      description: "A landing page with smooth and creative animations.",
+      stack: "Next.js, React, GSAP",
+      image: "/portrait.png",
+      tags: "Concept",
+      preview: "https://example.com/preview1",
+      github: "https://github.com/example/project-two",
+    },
+    project4: {
+      title: "E-Commerce Website; design from Figma Community",
+      description:
+        "Developed front-end of an e-commerce website from a Figma design.",
+      stack: "Next.js, React, Tailwind",
+      image: "/portrait.png",
+      tags: "Concept",
+      preview: "https://example.com/preview1",
+      github: "https://github.com/example/project-two",
+    },
+    project5: {
+      title: "Webflow Landing Page",
+      description:
+        "Engaging webflow website with rich animations utilizing power of GSAP.",
+      stack: "Webflow, GSAP",
+      image: "/portrait.png",
+      tags: "Concept",
+      preview: "https://example.com/preview1",
+      github: "https://github.com/example/project-two",
+    },
+    project6: {
+      title: "Custom Shopify Theme",
+      description:
+        "A custom shopify theme with rich animations and easy implementation.",
+      stack: "Liquid, Shopify, GSAP",
+      image: "/portrait.png",
+      tags: "Concept",
       preview: "https://example.com/preview1",
       github: "https://github.com/example/project-two",
     },
   };
 
   return (
-    <div className="flex h-fit w-full flex-col gap-6">
+    <div className="flex h-fit w-full flex-col gap-6 mb-20">
       <div className="font-primary p-6 text-6xl text-white md:p-8">
         <span className="text-(--grey)">Featured</span> Projects
       </div>
