b362a09d374b911651f9b286ad987cc598fdc932
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-06-24 02:05:14 +0800

    feat: add Cursor component to Home page for enhanced user experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,23 @@
+"use client"
+
 import TechStack from "@/components/TechStack";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
 import Navbar from "@/components/Navbar";
+import Cursor from "@/components/ui/Cursor";
 
 export default function Home() {
-	return (
-		<main className=" bg-[#121212] flex flex-col justify-center items-center overflow-hidden mx-auto">
-			<div className="max-w-7xl w-screen no-scrollbar">
-				<Navbar />
-				<Hero />
-				<TechStack />
-				<Projects />
-				<Contact />
-			</div>
-		</main>
-	);
+  return (
+    <main className="mx-auto flex flex-col items-center justify-center overflow-hidden bg-[#121212]">
+      <Cursor />
+      <div className="no-scrollbar w-screen max-w-7xl">
+        <Navbar />
+        <Hero />
+        <TechStack />
+        <Projects />
+        <Contact />
+      </div>
+    </main>
+  );
 }
